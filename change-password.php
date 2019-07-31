857c40c09f94b4182db2b3116790557d354b2c65
Xolani <xmgcoyi@gmail.com>
2019-07-31 21:15:27 +0200

    change password

--- change-password.php
+++ change-password.php
@@ -1,7 +1,7 @@
 <?php
-session_start();
-error_reporting(0);
-include('includes/config.php');
+
+include __DIR__ . '/bootstrap.php';
+
 if(strlen($_SESSION['alogin'])==0)
 	{	
 header('location:index.php');
@@ -13,19 +13,10 @@ if(isset($_POST['submit']))
 $password=md5($_POST['password']);
 $newpassword=md5($_POST['newpassword']);
 $username=$_SESSION['alogin'];
-$sql ="SELECT Password FROM users WHERE email=:username and password=:password";
-$query= $dbh -> prepare($sql);
-$query-> bindParam(':username', $username, PDO::PARAM_STR);
-$query-> bindParam(':password', $password, PDO::PARAM_STR);
-$query-> execute();
-$results = $query -> fetchAll(PDO::FETCH_OBJ);
-if($query -> rowCount() > 0)
+$usersGateway = new \Application\UsersGateway($dbh);
+if($usersGateway->countByUsernameAndPassword($username, $password) > 0)
 {
-$con="update users set password=:newpassword where email=:username";
-$chngpwd1 = $dbh->prepare($con);
-$chngpwd1-> bindParam(':username', $username, PDO::PARAM_STR);
-$chngpwd1-> bindParam(':newpassword', $newpassword, PDO::PARAM_STR);
-$chngpwd1->execute();
+$usersGateway->updatePasswordByUsername($username, $newpassword);
 $msg="Your Password succesfully changed";
 }
 else {
--- src/UsersGateway.php
+++ src/UsersGateway.php
@@ -18,6 +18,17 @@ class UsersGateway extends AbstractGateway
         return $query->rowCount();
     }
 
+    public function countByUsernameAndPassword($username, $password)
+    {
+        $sql ="SELECT Password FROM users WHERE email=:username and password=:password";
+        $query= $this->pdo -> prepare($sql);
+        $query-> bindParam(':username', $username, PDO::PARAM_STR);
+        $query-> bindParam(':password', $password, PDO::PARAM_STR);
+        $query-> execute();
+        $results = $query -> fetchAll(PDO::FETCH_OBJ);
+        return $query -> rowCount();
+    }
+
     public function update($name, $email, $mobileno, $designation, $image, $idedit)
     {
         $sql="UPDATE users SET name=(:name), email=(:email), mobile=(:mobileno), designation=(:designation), Image=(:image) WHERE id=(:idedit)";
@@ -29,5 +40,14 @@ class UsersGateway extends AbstractGateway
         $query-> bindParam(':image', $image, PDO::PARAM_STR);
         $query-> bindParam(':idedit', $idedit, PDO::PARAM_STR);
         $query->execute();
+    }
+
+    public function updatePasswordByUsername($username, $newpassword)
+    {
+        $con="update users set password=:newpassword where email=:username";
+        $chngpwd1 = $this->pdo->prepare($con);
+        $chngpwd1-> bindParam(':username', $username, PDO::PARAM_STR);
+        $chngpwd1-> bindParam(':newpassword', $newpassword, PDO::PARAM_STR);
+        $chngpwd1->execute();
     }
 }
