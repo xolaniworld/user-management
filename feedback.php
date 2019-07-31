fbeb97f98d7cc41ae587da747b630073158bed02
Xolani <xmgcoyi@gmail.com>
2019-07-31 21:04:41 +0200

    feedback gateway

--- feedback.php
+++ feedback.php
@@ -30,14 +30,8 @@ if(isset($_POST['submit']))
 	$notificationGateway = new \Application\NotificationGateway($dbh);
 	$notificationGateway->insertUserReciverType($user, $notireciver, $notitype);
 
-	$sql="insert into feedback (sender, reciver, title,feedbackdata,attachment) values (:user,:reciver,:title,:description,:attachment)";
-	$query = $dbh->prepare($sql);
-	$query-> bindParam(':user', $user, PDO::PARAM_STR);
-	$query-> bindParam(':reciver', $reciver, PDO::PARAM_STR);
-	$query-> bindParam(':title', $title, PDO::PARAM_STR);
-	$query-> bindParam(':description', $description, PDO::PARAM_STR);
-	$query-> bindParam(':attachment', $attachment, PDO::PARAM_STR);
-    $query->execute(); 
+	$feedbackGateway = new \Application\FeedbackGateway($dbh);
+	$feedbackGateway->insertSenderReciverTitleFeedbackAttachment($user, $reciver, $title, $description, $attachment);
 	$msg="Feedback Send";
 }    
 ?>
--- src/FeedbackGateway.php
+++ src/FeedbackGateway.php
@@ -4,7 +4,19 @@
 namespace Application;
 
 
+use PDO;
+
 class FeedbackGateway extends AbstractGateway
 {
-
+    public function insertSenderReciverTitleFeedbackAttachment($user, $reciver, $title, $description, $attachment)
+    {
+        $sql="insert into feedback (sender, reciver, title,feedbackdata,attachment) values (:user,:reciver,:title,:description,:attachment)";
+        $query = $this->pdo->prepare($sql);
+        $query-> bindParam(':user', $user, PDO::PARAM_STR);
+        $query-> bindParam(':reciver', $reciver, PDO::PARAM_STR);
+        $query-> bindParam(':title', $title, PDO::PARAM_STR);
+        $query-> bindParam(':description', $description, PDO::PARAM_STR);
+        $query-> bindParam(':attachment', $attachment, PDO::PARAM_STR);
+        $query->execute();
+    }
 }
