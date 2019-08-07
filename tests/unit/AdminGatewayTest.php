3b999642af62b58626fdde07673ccdbade64c27c
Xolani <xmgcoyi@gmail.com>
2019-08-07 20:59:41 +0200

    admin gateway tests

--- tests/unit/AdminGatewayTest.php
+++ tests/unit/AdminGatewayTest.php
@@ -7,54 +7,48 @@ class AdminGatewayTest extends \PHPUnit\Framework\TestCase
     private $pdo;
     private $id;
 
-    
     public function setUp(): void
     {
         $this->pdo = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASS, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
         $this->gateway = new \Application\AdminGateway($this->pdo);
 
-
-        $this->pdo->query("insert into admin(email, username, Password) values ('test@test.com', 'test-admin','test')");
-        $this->id = $this->pdo->lastInsertId();
+        $this->insertTestUser();
     }
 
-    public function testCountByEmailAndPassword()
+    public function testCountByUsernameAndPassword()
     {
         $this->assertTrue($this->gateway->countByUsernameAndPassword('test-admin', 'test') > 0);
     }
 
-//    public function countPasswordByPasswordAndUsername($username, $password)
-//    {
-//        $sql = "SELECT Password FROM admin WHERE UserName=:username and Password=:password";
-//        $query = $this->pdo->prepare($sql);
-//        $query->bindParam(':username', $username, PDO::PARAM_STR);
-//        $query->bindParam(':password', $password, PDO::PARAM_STR);
-//        $query->execute();
-//        $results = $query->fetchAll(PDO::FETCH_OBJ);
-//
-//        return $query->rowCount();
-//    }
-//
-//    public function updatePasswordByUsername($username, $newpassword)
-//    {
-//        $con = "update admin set Password=:newpassword where UserName=:username";
-//        $chngpwd1 = $this->pdo->prepare($con);
-//        $chngpwd1->bindParam(':username', $username, PDO::PARAM_STR);
-//        $chngpwd1->bindParam(':newpassword', $newpassword, PDO::PARAM_STR);
-//        $chngpwd1->execute();
-//    }
-//
-//    public function updateUsernameAndEmail($name, $email)
-//    {
-//        $sql="UPDATE admin SET username=(:name), email=(:email)";
-//        $query = $this->pdo->prepare($sql);
-//        $query-> bindParam(':name', $name, PDO::PARAM_STR);
-//        $query-> bindParam(':email', $email, PDO::PARAM_STR);
-//        $query->execute();
-//    }
+    public function testCountPasswordByPasswordAndUsername()
+    {
+        $this->assertTrue(
+            $this->gateway->countPasswordByPasswordAndUsername('test-admin', 'test') > 0
+        );
+    }
+
+    public function testUpdatePasswordByUsername()
+    {
+        $this->gateway->updatePasswordByUsername('test-admin', 'foo');
+
+        $result = $this->pdo->query("select password from admin where username='test-admin'", PDO::FETCH_OBJ);
+        $this->assertEquals('foo', $result->fetch()->password );
+    }
+
+    public function testUpdateUsernameAndEmail()
+    {
+        //not sure how to test this
+        $this->assertTrue(true);
+    }
 
     public function tearDown(): void
     {
-//        $this->pdo->query("delete from admin where id=" . $this->id);
+        $this->pdo->query("delete from admin where id=" . $this->id);
+    }
+
+    private function insertTestUser()
+    {
+        $this->pdo->query("insert into admin(email, username, Password) values ('test@test.com', 'test-admin','test')");
+        $this->id = $this->pdo->lastInsertId();
     }
 }
