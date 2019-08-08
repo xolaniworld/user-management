0aed232762bc97974010a240743d61e0ab33f37a
Xolani <xmgcoyi@gmail.com>
2019-08-08 11:48:11 +0200

    Notifcation test

---
+++ tests/unit/DeletedUserGatewayTest.php
@@ -0,0 +1,50 @@
+<?php
+
+
+class DeletedUserGateway extends \PHPUnit\Framework\TestCase
+{
+    private $gateway;
+    private $pdo;
+    private $id;
+
+    public function setUp(): void
+    {
+        $this->pdo = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASS, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+        $this->gateway = new \Application\DeletedUserGateway($this->pdo);
+
+        $this->insertTestUser();
+    }
+
+    public function testCountById()
+    {
+        $this->assertTrue(
+            $this->gateway->countById() > 0
+        );
+    }
+
+    public function testInsertByName()
+    {
+        $this->gateway->insertByName('foo');
+
+        $sth = $this->pdo->query("select email from deleteduser where email='foo'", PDO::FETCH_OBJ);
+        $this->assertEquals('foo', $sth->fetch()->email);
+    }
+
+    public function testFindAll()
+    {
+        list($results, $count) = $this->gateway->findAll();
+        $this->assertTrue(is_array($results));
+        $this->assertTrue($count > 0);
+    }
+
+    public function tearDown(): void
+    {
+        $this->pdo->query("delete from admin where id=" . $this->id);
+    }
+
+    private function insertTestUser()
+    {
+        $this->pdo->query("insert into deleteduser(email) values ('test@test.com')");
+        $this->id = $this->pdo->lastInsertId();
+    }
+}
---
+++ tests/unit/NotificationGatewayTest.php
@@ -0,0 +1,56 @@
+<?php
+
+class NotificationGateway extends \PHPUnit\Framework\TestCase
+{
+    private $gateway;
+    private $pdo;
+    private $id;
+
+    public function setUp(): void
+    {
+        $this->pdo = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASS, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+        $this->gateway = new \Application\NotificationGateway($this->pdo);
+
+        $this->insertTestUser();
+    }
+
+    public function testInsertUserReciverType()
+    {
+
+        $this->gateway->insertUserReciverType('foo', 'bar', 'baz');
+        $id = $this->pdo->lastInsertId();
+        $sth = $this->pdo->query('select notiuser, notireciver, notitype from notification where id=' . $id, PDO::FETCH_OBJ);
+        $obj = $sth->fetch();
+
+        $this->assertEquals('foo', $obj->notiuser);
+        $this->assertEquals('bar', $obj->notireciver);
+        $this->assertEquals('baz', $obj->notitype);
+
+        $this->pdo->exec('delete from notification where id='.$id);
+    }
+
+    public function testCountByReciver()
+    {
+        $this->assertTrue(
+            $this->gateway->countByReciver('foo') > 0
+        );
+    }
+
+    public function testFindByNotiReciver()
+    {
+        list($results, $count) = $this->gateway->findByNotiReciver('foo');
+        $this->assertTrue(is_array($results));
+        $this->assertTrue($count > 0);
+    }
+
+    public function tearDown(): void
+    {
+        $this->pdo->query("delete from notification where id=" . $this->id);
+    }
+    
+    private function insertTestUser()
+    {
+        $this->pdo->query("insert into notification (notireciver, notiuser, notitype) values ('foo', 'bar', 'baz')");
+        $this->id = $this->pdo->lastInsertId();
+    }
+}
