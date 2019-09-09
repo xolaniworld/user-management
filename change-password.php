d995ac15955f0dd832f8b4ae7c4f60e4231576b9
Xolani <xmgcoyi@gmail.com>
2019-09-09 12:36:12 +0200

    change password

--- change-password.php
+++ change-password.php
@@ -24,94 +24,9 @@ if(\Application\Authentication::isNotLoggedIn()) {
 
     $headerMeta = '<meta name="theme-color" content="#3e454c">';
     $headerTitle = 'User Change Password';
-?>
-	<?php include('includes/html_header.php');?>
-	<?php include('includes/header.php');?>
-	<div class="ts-main-content">
-	<?php include('includes/leftbar.php');?>
-		<div class="content-wrapper">
-			<div class="container-fluid">
-				<div class="row">
-					<div class="col-md-12">
-						<h2 class="page-title">Change Password</h2>
 
-						<div class="row">
-							<div class="col-md-10">
-								<div class="panel panel-default">
-									<div class="panel-heading">Form fields</div>
-									<div class="panel-body">
-										<form method="post" name="chngpwd" class="form-horizontal" onSubmit="return valid();">
-                                            <?php include INCLUDES_DIR . 'alerts.php'?>
-											<div class="form-group">
-												<label class="col-sm-4 control-label">Current Password</label>
-												<div class="col-sm-8">
-													<input type="password" class="form-control" name="password" id="password" required>
-												</div>
-											</div>
-											<div class="hr-dashed"></div>
-
-											<div class="form-group">
-												<label class="col-sm-4 control-label">New Password</label>
-												<div class="col-sm-8">
-													<input type="password" class="form-control" name="newpassword" id="newpassword" required>
-												</div>
-											</div>
-											<div class="hr-dashed"></div>
-
-											<div class="form-group">
-												<label class="col-sm-4 control-label">Confirm Password</label>
-												<div class="col-sm-8">
-													<input type="password" class="form-control" name="confirmpassword" id="confirmpassword" required>
-												</div>
-											</div>
-											<div class="hr-dashed"></div>
-
-
-
-											<div class="form-group">
-												<div class="col-sm-8 col-sm-offset-4">
-
-													<button class="btn btn-primary" name="submit" type="submit">Save changes</button>
-												</div>
-											</div>
-
-										</form>
-
-									</div>
-								</div>
-							</div>
-
-						</div>
-
-
-
-					</div>
-				</div>
-
-
-			</div>
-		</div>
-	</div>
-
-	<!-- Loading Scripts -->
-	<script src="js/jquery.min.js"></script>
-	<script src="js/bootstrap-select.min.js"></script>
-	<script src="js/bootstrap.min.js"></script>
-	<script src="js/jquery.dataTables.min.js"></script>
-	<script src="js/dataTables.bootstrap.min.js"></script>
-	<script src="js/Chart.min.js"></script>
-	<script src="js/fileinput.js"></script>
-	<script src="js/chartData.js"></script>
-	<script src="js/main.js"></script>
-	<script type="text/javascript">
-				 $(document).ready(function () {
-					setTimeout(function() {
-						$('.succWrap').slideUp("slow");
-					}, 3000);
-					});
-	</script>
-
-</body>
-
-</html>
-<?php }
+    // Render a template
+    echo $templates->render('change_password', [
+        'alogin' => $_SESSION['alogin'],
+    ]);
+}
---
+++ templates/change_password.php
@@ -0,0 +1,61 @@
+<?php $this->layout('layouts/frontend', [
+    'title' => 'User Change Password',
+    'alogin' => $alogin
+]) ?>
+<h2 class="page-title">Change Password</h2>
+
+<div class="row">
+    <div class="col-md-10">
+        <div class="panel panel-default">
+            <div class="panel-heading">Form fields</div>
+            <div class="panel-body">
+                <form method="post" name="chngpwd" class="form-horizontal" onSubmit="return valid();">
+                    <?= $this->insert('partials/alerts') ?>
+                    <div class="form-group">
+                        <label class="col-sm-4 control-label">Current Password</label>
+                        <div class="col-sm-8">
+                            <input type="password" class="form-control" name="password" id="password" required>
+                        </div>
+                    </div>
+                    <div class="hr-dashed"></div>
+
+                    <div class="form-group">
+                        <label class="col-sm-4 control-label">New Password</label>
+                        <div class="col-sm-8">
+                            <input type="password" class="form-control" name="newpassword" id="newpassword" required>
+                        </div>
+                    </div>
+                    <div class="hr-dashed"></div>
+
+                    <div class="form-group">
+                        <label class="col-sm-4 control-label">Confirm Password</label>
+                        <div class="col-sm-8">
+                            <input type="password" class="form-control" name="confirmpassword" id="confirmpassword" required>
+                        </div>
+                    </div>
+                    <div class="hr-dashed"></div>
+                    <div class="form-group">
+                        <div class="col-sm-8 col-sm-offset-4">
+
+                            <button class="btn btn-primary" name="submit" type="submit">Save changes</button>
+                        </div>
+                    </div>
+
+                </form>
+
+            </div>
+        </div>
+    </div>
+
+</div>
+
+
+<?php $this->push('before_body_close') ?>
+<script type="text/javascript">
+    $(document).ready(function () {
+        setTimeout(function () {
+            $('.succWrap').slideUp("slow");
+        }, 3000);
+    });
+</script>
+<?php $this->end() ?>
