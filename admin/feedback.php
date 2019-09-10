5be498f176ee1e24b495acce61bef05ef629fd40
Xolani <xmgcoyi@gmail.com>
2019-09-10 10:54:51 +0200

    render feedback

--- admin/feedback.php
+++ admin/feedback.php
@@ -1,164 +1,39 @@
 <?php
 include __DIR__ . '/../bootstrap.php';
 
-if(\Application\Authentication::isNotLoggedIn()) {
+if (\Application\Authentication::isNotLoggedIn()) {
     header('location:index.php');
 } else {
     $userGateway = new \Application\UsersGateway($dbh);
     $request = new \Application\Request();
     $filesystem = new \Application\Filesystem(IMAGES_DIR);
     $usersTransaction = new Application\Users\UsersTransactions($userGateway, $request, $filesystem);
-//    $msg = $usersTransaction->updateFeedback();
-    if(isset($_GET['del'])) {
+
+    $msg = null;
+
+    if (isset($_GET['del'])) {
         $usersTransaction->deleteUserById();
-        $msg="Data Deleted successfully";
+        $msg = "Data Deleted successfully";
     }
 
-    if(isset($_REQUEST['unconfirm'])) {
+    if (isset($_REQUEST['unconfirm'])) {
         $usersTransaction->updateStatusUnConfirmed();
 
-        $msg="Changes Sucessfully";
+        $msg = "Changes Sucessfully";
     }
 
-    if(isset($_REQUEST['confirm'])) {
+    if (isset($_REQUEST['confirm'])) {
         $usersTransaction->updateStatusConfirmed();
 
-        $msg="Changes Sucessfully";
+        $msg = "Changes Sucessfully";
     }
-?>
-<!doctype html>
-<html lang="en" class="no-js">
 
-<head>
-	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
-	<meta name="description" content="">
-	<meta name="author" content="">
-	<meta name="theme-color" content="#3e454c">
-	
-	<title>Manage Feedback</title>
+    $feedbackGateway = new \Application\FeedbackGateway($dbh);
+    $feedbackTransaction = new \Application\FeedbackTransaction($feedbackGateway);
+    $feedbackTransaction->findAdmin();
+    $results = $feedbackTransaction->getFeedback();
+    $count = $feedbackTransaction->getTotal();
+    $cnt = 1;
 
-	<!-- Font awesome -->
-	<link rel="stylesheet" href="css/font-awesome.min.css">
-	<!-- Sandstone Bootstrap CSS -->
-	<link rel="stylesheet" href="css/bootstrap.min.css">
-	<!-- Bootstrap Datatables -->
-	<link rel="stylesheet" href="css/dataTables.bootstrap.min.css">
-	<!-- Bootstrap social button library -->
-	<link rel="stylesheet" href="css/bootstrap-social.css">
-	<!-- Bootstrap select -->
-	<link rel="stylesheet" href="css/bootstrap-select.css">
-	<!-- Bootstrap file input -->
-	<link rel="stylesheet" href="css/fileinput.min.css">
-	<!-- Awesome Bootstrap checkbox -->
-	<link rel="stylesheet" href="css/awesome-bootstrap-checkbox.css">
-	<!-- Admin Stye -->
-	<link rel="stylesheet" href="css/style.css">
-  <style>
-
-	.errorWrap {
-    padding: 10px;
-    margin: 0 0 20px 0;
-	background: #dd3d36;
-	color:#fff;
-    -webkit-box-shadow: 0 1px 1px 0 rgba(0,0,0,.1);
-    box-shadow: 0 1px 1px 0 rgba(0,0,0,.1);
+    echo $templates->render('admin/feedback', compact('msg', 'results', 'count', 'cnt'));
 }
-.succWrap{
-    padding: 10px;
-    margin: 0 0 20px 0;
-	background: #5cb85c;
-	color:#fff;
-    -webkit-box-shadow: 0 1px 1px 0 rgba(0,0,0,.1);
-    box-shadow: 0 1px 1px 0 rgba(0,0,0,.1);
-}
-
-		</style>
-
-</head>
-
-<body>
-	<?php include('includes/header.php');?>
-
-	<div class="ts-main-content">
-		<?php include('includes/leftbar.php');?>
-		<div class="content-wrapper">
-			<div class="container-fluid">
-
-				<div class="row">
-					<div class="col-md-12">
-
-						<h2 class="page-title">Manage Feedback</h2>
-
-						<!-- Zero Configuration Table -->
-						<div class="panel panel-default">
-							<div class="panel-heading">List Users</div>
-							<div class="panel-body">
-                                <?php include INCLUDES_DIR . 'alerts_mgshow.php'; ?>
-                                <table id="zctb" class="display table table-striped table-bordered table-hover" cellspacing="0" width="100%">
-									<thead>
-										<tr>
-										       <th>#</th>
-												<th>User Email</th>
-												<th>Title</th>
-                                                <th>Feedback</th>
-                                                <th>Attachment</th>
-											    <th>Action</th>	
-										</tr>
-									</thead>
-									
-									<tbody>
-
-<?php 
-$reciver = 'Admin';
-$feedbackGateway = new \Application\FeedbackGateway($dbh);
-$feedbackTransaction = new \Application\FeedbackTransaction($feedbackGateway);
-$feedbackTransaction->findAdmin();
-$results = $feedbackTransaction->getFeedback();
-$count = $feedbackTransaction->getTotal();
-$cnt=1;
-if($count > 0) {
-foreach($results as $result)
-{?>
-										<tr>
-											<td><?php echo htmlentities($cnt);?></td>
-                                            <td><?php echo htmlentities($result->sender);?></td>
-											<td><?php echo htmlentities($result->title);?></td>
-                                            <td><?php echo htmlentities($result->feedbackdata);?></td>
-                                            <td><a href="../attachment/<?php echo htmlentities($result->attachment);?>" ><?php echo htmlentities($result->attachment);?></a></td>
-<td>
-<a href="sendreply.php?reply=<?php echo $result->sender;?>">&nbsp; <i class="fa fa-mail-reply"></i></a>&nbsp;&nbsp;
-</td>
-										</tr>
-										<?php $cnt++; }} ?>
-										
-									</tbody>
-								</table>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-	<!-- Loading Scripts -->
-	<script src="js/jquery.min.js"></script>
-	<script src="js/bootstrap-select.min.js"></script>
-	<script src="js/bootstrap.min.js"></script>
-	<script src="js/jquery.dataTables.min.js"></script>
-	<script src="js/dataTables.bootstrap.min.js"></script>
-	<script src="js/Chart.min.js"></script>
-	<script src="js/fileinput.js"></script>
-	<script src="js/chartData.js"></script>
-	<script src="js/main.js"></script>
-	<script type="text/javascript">
-				 $(document).ready(function () {          
-					setTimeout(function() {
-						$('.succWrap').slideUp("slow");
-					}, 3000);
-					});
-		</script>
-</body>
-</html>
-<?php }
