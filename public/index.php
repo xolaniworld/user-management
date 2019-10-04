1796a3424289317d6172096d20d57a3268efc95a
Xolani <xmgcoyi@gmail.com>
2019-10-04 09:42:18 +0200

    removed bootstrap from public)

--- public/index.php
+++ public/index.php
@@ -1,10 +1,5 @@
 <?php
 
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Route;
-use Symfony\Component\Routing\RouteCollection;
-
 include dirname(__DIR__) . '/bootstrap.php';
 
 $app = new \Application\Application();
