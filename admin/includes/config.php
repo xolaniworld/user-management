82cb6863e07fe40fa58a8c40779618756fd90220
Xolani Mgcoyi <xolani.mgcoyi@mukuru.com>
2023-12-06 13:35:39 +0200

    use common config.php

--- admin/includes/config.php
+++ admin/includes/config.php
@@ -1,17 +1,3 @@
 ﻿<?php
-// DB credentials.
-//define('DB_HOST','192.168.16.2');
-define('DB_HOST','mysqldb');
-define('DB_USER','useradmin');
-define('DB_PASS','S3cr3t');
-define('DB_NAME','thedatabase');
-// Establish database connection.
-try
-{
-$dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER, DB_PASS,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
-}
-catch (PDOException $e)
-{
-exit("Error: " . $e->getMessage());
-}
 
+require __DIR__ . '/../../includes/config.php';
