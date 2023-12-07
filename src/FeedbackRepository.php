9a3a8d19618183626fcdc24270e7339dbf1ec50a
Xolani Mgcoyi <xolani.mgcoyi@mukuru.com>
2023-12-07 10:39:38 +0200

    select all by receiver

--- src/FeedbackRepository.php
+++ src/FeedbackRepository.php
@@ -4,6 +4,15 @@ namespace UserManagement;
 use PDO;
 class FeedbackRepository extends AbstractRepository
 {
+    public function selectAllByReceiver($reciver)
+    {
+        $sql = "SELECT * from  feedback where reciver = (:reciver)";
+        $query = $this->dbh -> prepare($sql);
+        $query-> bindParam(':reciver', $reciver, PDO::PARAM_STR);
+        $query->execute();
+        $this->results=$query->fetchAll(PDO::FETCH_OBJ);
+        return $query->rowCount() > 0;
+    }
     public function insert($user, $reciver, $title, $description, $attachment)
     {
         $sql="insert into feedback (sender, reciver, title,feedbackdata,attachment) values (:user,:reciver,:title,:description,:attachment)";
