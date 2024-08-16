fc29918830ad32eb8297f1093b67dce94cc6dc19
Xolani Mgcoyi <xolani.mgcoyi@clicksgroup.co.za>
2024-08-16 18:37:37 +0200

    migration

--- db/migrations/20240816153411_my_first_migration.php
+++ db/migrations/20240816153411_my_first_migration.php
@@ -19,6 +19,58 @@ final class MyFirstMigration extends AbstractMigration
      */
     public function change(): void
     {
+        // create the table
+        $table = $this->table('admin');
+        $table->addColumn('id', 'integer')
+            ->addColumn('username', 'varchar(250)')
+            ->addColumn('email', 'varchar(250)')
+            ->addColumn('password', 'varchar(250)')
+            ->addColumn('created', 'datetime')
+            ->addColumn('updated', 'datetime')
+            ->create();
+
+        $table = $this->table('deleteduser');
+        $table->addColumn('id', 'integer')
+            ->addColumn('email', 'varchar(250)')
+            ->addColumn('deltime', 'datetime')
+            ->addColumn('created', 'datetime')
+            ->addColumn('updated', 'datetime')
+            ->create();
+
+        $table = $this->table('feedback');
+        $table->addColumn('id', 'integer')
+            ->addColumn('sender', 'varchar(250)')
+            ->addColumn('receiver', 'varchar(250)')
+            ->addColumn('title', 'varchar(250)')
+            ->addColumn('feedback_data', 'varchar(250)')
+            ->addColumn('attachment', 'varchar(250)')
+            ->addColumn('created', 'datetime')
+            ->addColumn('updated', 'datetime')
+            ->create();
+
+        $table = $this->table('notification');
+        $table->addColumn('id', 'integer')
+            ->addColumn('notification_user', 'varchar(250)')
+            ->addColumn('notification_receiver', 'varchar(250)')
+            ->addColumn('notification_type', 'varchar(250)')
+            ->addColumn('time', 'datetime')
+            ->addColumn('created', 'datetime')
+            ->addColumn('updated', 'datetime')
+            ->create();
+
+        $table = $this->table('users');
+        $table->addColumn('id', 'integer')
+            ->addColumn('name', 'varchar(250)')
+            ->addColumn('email', 'varchar(250)')
+            ->addColumn('password', 'varchar(250)')
+            ->addColumn('gender', 'varchar(250)')
+            ->addColumn('mobile', 'varchar(250)')
+            ->addColumn('designation', 'varchar(250)')
+            ->addColumn('image', 'varchar(250)')
+            ->addColumn('status', 'varchar(250)')
+            ->addColumn('created', 'datetime')
+            ->addColumn('updated', 'datetime')
+            ->create();
 
     }
 }
